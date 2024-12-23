e0ec6abbda4158e36407ffcfcb1b288de264eb39
Rafa Wierzba <rafaelwierzba@gmail.com>
2024-12-23 17:21:42 -0300

    Update upload_youtube.py

--- scripts/upload_youtube.py
+++ scripts/upload_youtube.py
@@ -1,29 +1,41 @@
 # scripts/upload_youtube.py
 
+import logging
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-import logging
 
-def upload_video_to_youtube(youtube, video_path, title, description, category_id="22", privacy_status="public"):
+def upload_video_to_youtube(youtube, video_path, title, description, category_id=22, keywords=""):
     try:
-        logging.info(f"Iniciando upload para o YouTube: {title}")
+        logging.info(f"Iniciando upload do vídeo: {video_path}")
+
+        request_body = {
+            'snippet': {
+                'categoryId': category_id,
+                'title': title,
+                'description': description,
+                'tags': [tag.strip() for tag in keywords.split(",")] if keywords else []
+            },
+            'status': {
+                'privacyStatus': 'public',  # Pode ser 'public', 'private' ou 'unlisted'
+            }
+        }
+
+        media = MediaFileUpload(video_path, chunksize=-1, resumable=True)
+
         request = youtube.videos().insert(
             part="snippet,status",
-            body={
-                "snippet": {
-                    "title": title,
-                    "description": description,
-                    "tags": ["automacao", "video", "youtube"],
-                    "categoryId": category_id
-                },
-                "status": {
-                    "privacyStatus": privacy_status
-                }
-            },
-            media_body=MediaFileUpload(video_path, resumable=True)
+            body=request_body,
+            media_body=media
         )
-        response = request.execute()
-        logging.info(f"Vídeo enviado para o YouTube com sucesso! ID: {response['id']}")
+
+        response = None
+        while response is None:
+            status, response = request.next_chunk()
+            if status:
+                logging.info(f"Progresso do upload: {int(status.progress() * 100)}%")
+
+        logging.info(f"Vídeo enviado com sucesso. ID do Vídeo: {response['id']}")
+
     except Exception as e:
-        logging.error(f"Erro no upload para o YouTube: {e}")
+        logging.error(f"Erro ao enviar o vídeo para o YouTube: {e}")
         raise

9f7e50cdb97fb08fb9b56bef149747bc82f95aff
Rafa Wierzba <rafaelwierzba@gmail.com>
2025-01-28 15:23:52 -0300

    Update youtube_auth.py

--- scripts/youtube_auth.py
+++ scripts/youtube_auth.py
@@ -1,10 +1,12 @@
+import os
+import json
+import base64
 import logging
+
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
-import os
-import json
 
 SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 
@@ -12,6 +14,11 @@ def load_credentials(client_secret_path, token_path):
     """
     Carrega as credenciais do OAuth2.
     """
+    with open(client_secret_path, 'r') as file:
+        base64_content = file.read()
+    json_content = base64.b64decode(base64_content).decode('utf-8')
+    client_secret = json.loads(json_content)
+
     creds = None
     if os.path.exists(token_path):
         try:
@@ -25,9 +32,9 @@ def load_credentials(client_secret_path, token_path):
             except Exception as e:
                 logging.error(f"Erro ao atualizar o token: {e}")
                 creds = None
-        if not creds:
-            flow = InstalledAppFlow.from_client_secrets_file(client_secret_path, SCOPES)
-            creds = flow.run_console()
-            with open(token_path, 'w') as token:
-                token.write(creds.to_json())
+            if not creds:
+                flow = InstalledAppFlow.from_client_config(client_secret, SCOPES)
+                creds = flow.run_console()
+                with open(token_path, 'w') as token:
+                    token.write(creds.to_json())
     return creds
